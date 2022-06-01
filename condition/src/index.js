fdf1eb443903fff5df762db993c1950d6deb5494
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-06-01 18:53:12 +0530

    add

--- condition/src/index.js
+++ condition/src/index.js
@@ -1,67 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Component from "./component";
 import First from "./Component/First";
 import Second from "./Component/Second";
+import axios from "axios";
 
 const App = () => {
+  const [applicationCode, setCode] = useState([]);
   const onClickHandler = () => {
-    alert(" I am from main app");
+    const code = async () => {
+      try {
+        let res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        console.log(res);
+      } catch {
+        console.log(`error`);
+      }
+    };
+    code();
   };
   return (
     <div>
-      <Component vaishanvi={onClickHandler} />
-      <Second neha={onClickHandler} />
-      <First pratik={onClickHandler} />
+      Hi
+      <button onClick={onClickHandler}> Click here </button>
+      <label htmlFor=""> Select the value from dropdown</label>
+      <select name="" id="">
+        <option value=""></option>
+        <option value="1210"> 120</option>
+        <option value="12510"></option>
+        <option value="12056"></option>
+      </select>
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-// {/* <Component name="vaishnavi">
-// <div>
-// //   <label htmlFor=""> Enter your firstName</label>
-// //   <button> Update</button>
-// // </div> */
-// // }
-// // <First />;
-// // // </Component> */} */}
-
-// // const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// //
-// // console.log(obj);
-// // console.log(obj.firstName);
-// // console.log(obj.lastName);
-// // console.log(array);
-
-// // const arrayObj = [
-// //   {
-// //     firstName: " Vaishnavi",
-// //     lastName: " Thakare",
-// //   },
-// //   {
-// //     firstName: "nikita",
-// //     lastName: " Sawant",
-// //   },
-// //   {
-// //     firstName: " Pranjali",
-// //     lastName: "Gawande",
-// //   },
-// //   {
-// //     firstName: "Payal",
-// //     lastName: "Gandhi",
-// //   },
-// //   {
-// //     firstName: "kanika",
-// //     lastName: "Raut",
-// //   },
-// // ];
-// // console.log(arrayObj[0].firstName);
-
-// // const data = [
-// //   { firstName: "Vaishnavi", lastName: "Thakare" },
-// //   { firstName: "Nikita", lastName: "Sawant" },
-// // ];
-// // console.log(data.firstName)
